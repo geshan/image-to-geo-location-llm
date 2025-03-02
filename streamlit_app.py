158bfb628c9debd226cab035a3964ebb86f049ed
Geshan Manandhar <geshan@simplywallst.com>
2025-03-02 17:13:25 +1100

    Get project and region from the user

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,9 +35,6 @@ if uploaded_file is not None:
         PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT")
         LOCATION = os.environ.get("GOOGLE_CLOUD_REGION")
 
-        print("All available environmentVariables:")
-        print(os.environ)
-
         client = genai.Client(
             vertexai=True,
             project=PROJECT_ID,
