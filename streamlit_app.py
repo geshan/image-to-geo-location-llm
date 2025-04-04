1f31eaeef43581aed6e2b6a45778d890a3a9db1a
Geshan Manandhar <geshan@simplywallst.com>
2025-04-04 13:54:33 +1100

    Do a sync call to Gemini to show proper Google maps link
    - [x] Adds a spinner

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,56 +32,56 @@ if uploaded_file is not None:
 
     if generate:
         st.write("let's guess the location now")
-        PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT")
-        LOCATION = os.environ.get("GOOGLE_CLOUD_REGION")
+        with st.spinner("Generating..."):
+            PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT")
+            LOCATION = os.environ.get("GOOGLE_CLOUD_REGION")
 
-        client = genai.Client(
-            vertexai=True,
-            project=PROJECT_ID,
-            location=LOCATION,
-        )
-
-        text1 = types.Part.from_text(text="""You are an OSINT investigator. Your job is to geolocate where the photos are taken. Provide the country, region, and city name of the location. Please pinpoint the exact location with latitude and longitude where the photo was taken.  
-
-        Could you always explain your methodology and how you concluded? Provide steps to verify your work. Also, mention the percentage of how sure you are of the place you have identified it to be and add a Google Maps link to the exact location.""")
-        image1 = types.Part.from_bytes(
-            data=bytes_data,
-            mime_type="image/jpeg"
-        )
-
-        model = "gemini-2.0-flash-lite-001"
-        contents = [
-            types.Content(
-            role="user",
-            parts=[
-                text1,
-                image1
-            ]
+            client = genai.Client(
+                vertexai=True,
+                project=PROJECT_ID,
+                location=LOCATION,
             )
-        ]
-        generate_content_config = types.GenerateContentConfig(
-            temperature = 0.1,
-            top_p = 0.5,
-            max_output_tokens = 4096,
-            response_modalities = ["TEXT"],
-            safety_settings = [types.SafetySetting(
-            category="HARM_CATEGORY_HATE_SPEECH",
-            threshold="OFF"
-            ),types.SafetySetting(
-            category="HARM_CATEGORY_DANGEROUS_CONTENT",
-            threshold="OFF"
-            ),types.SafetySetting(
-            category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
-            threshold="OFF"
-            ),types.SafetySetting(
-            category="HARM_CATEGORY_HARASSMENT",
-            threshold="OFF"
-            )],
-        )
-    
-        for chunk in client.models.generate_content_stream(
-            model = model,
-            contents = contents,
-            config = generate_content_config,
-            ):
-            st.write(chunk.text)
+
+            text1 = types.Part.from_text(text="""You are an OSINT investigator. Your job is to geolocate where the photos are taken. Provide the country, region, and city name of the location. Please pinpoint the exact location with latitude and longitude where the photo was taken.  
+
+            Could you always explain your methodology and how you concluded? Provide steps to verify your work. Also, mention the percentage of how sure you are of the place you have identified it to be and add a Google Maps link having google.com/maps to the exact location.""")
+            image1 = types.Part.from_bytes(
+                data=bytes_data,
+                mime_type="image/jpeg"
+            )
+
+            model = "gemini-2.0-flash-lite-001"
+            contents = [
+                types.Content(
+                role="user",
+                parts=[
+                    text1,
+                    image1
+                ]
+                )
+            ]
+            generate_content_config = types.GenerateContentConfig(
+                temperature = 0.1,
+                top_p = 0.5,
+                max_output_tokens = 4096,
+                response_modalities = ["TEXT"],
+                safety_settings = [types.SafetySetting(
+                category="HARM_CATEGORY_HATE_SPEECH",
+                threshold="OFF"
+                ),types.SafetySetting(
+                category="HARM_CATEGORY_DANGEROUS_CONTENT",
+                threshold="OFF"
+                ),types.SafetySetting(
+                category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                threshold="OFF"
+                ),types.SafetySetting(
+                category="HARM_CATEGORY_HARASSMENT",
+                threshold="OFF"
+                )],
+            )
+        
+            response = client.models.generate_content(
+            model = model, contents= contents, config = generate_content_config
+            )
+            st.write(response.text)
+        st.success("Done!")
